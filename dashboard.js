09fd0f46adb9d145ae2b87e16b94d0f164eacba7
Simon Wunderlin <simon.wunderlin@usb.ch>
2016-11-15 09:38:10 +0100

    verified critical state filter

--- dashboard.js
+++ dashboard.js
@@ -28,7 +28,7 @@ dashboard.factory("globals", function($window, $http) {
 	// dev env uses a script (fetch.sh) to get json from server
 	if (dev) {
 		factory.hosts.location = "hosts.json"
-		factory.services.location = "services-soft.json"
+		factory.services.location = "services.json"
 	}
 	
 	function success_callback_services(response) {
@@ -131,6 +131,51 @@ function _appController($scope, $window, globals, $timeout) {
 	}
 	
 	reload();
+	
+	/*
+	// 2. This code loads the IFrame Player API code asynchronously.
+	var tag = document.createElement('script');
+	tag.src = "https://www.youtube.com/iframe_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+	// 3. This function creates an <iframe> (and YouTube player)
+	//    after the API code downloads.
+	var player;
+	function onYouTubeIframeAPIReady() {
+		player = new YT.Player('bgvideo', {
+			height: '100%',
+			width: '100%',
+			videoId: 'RDfjXj5EGqI',
+			events: {
+				'onReady': onPlayerReady,
+				'onStateChange': onPlayerStateChange
+			}
+		});
+	}
+
+	// 4. The API will call this function when the video player is ready.
+	function onPlayerReady(event) {
+		console.log("start")
+		event.target.playVideo();
+	}
+
+	// 5. The API calls this function when the player's state changes.
+	//    The function indicates that when playing a video (state=1),
+	//    the player should play for six seconds and then stop.
+	var done = false;
+	function onPlayerStateChange(event) {
+		if (event.data == YT.PlayerState.PLAYING && !done) {
+			setTimeout(stopVideo, 6000);
+			done = true;
+		}
+	}
+	function stopVideo() {
+		player.stopVideo();
+	}
+	
+	//onYouTubeIframeAPIReady();
+	*/
 }
 
 dashboard.controller("appController", ['$scope', '$window', 'globals', '$timeout', _appController]);
